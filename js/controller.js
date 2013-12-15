3b9d6d58dc0a34832a49c2aab16be7e8ded94388
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2013-12-15 22:46:22 +0900

    clear keyword after search

--- js/controller.js
+++ js/controller.js
@@ -1,11 +1,10 @@
 var mainCtrl = function($scope) {
     $scope.playlists = [];
     $scope.searchPlaylist = function() {
-        var searchResults = document.getElementById('searchResult');
-        angular.element(searchResults).empty();
         SC.get('/playlists', { q: $scope.keyword }, function(playlists) {
             for (var i in playlists) {
                 $scope.playlists.push(playlists[i]);
+                $scope.keyword = '';
             }
         });
     }
