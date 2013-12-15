9d0f89db0019dcc61608d5ab1faed37f497c7041
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2013-12-16 01:15:40 +0900

    clear result before search

--- js/controller.js
+++ js/controller.js
@@ -1,6 +1,7 @@
 var mainCtrl = function($scope) {
     $scope.playlists = [];
     $scope.searchPlaylists = function() {
+        $scope.playlists = [];
         SC.get('/playlists', { q: $scope.keyword }, function(playlists) {
             for (var i in playlists) {
                 $scope.playlists.push(playlists[i]);
