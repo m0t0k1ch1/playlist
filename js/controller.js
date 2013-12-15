36e777f690b83ddcb45a024b42cc3302e9d2bf4a
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2013-12-15 19:02:57 +0900

    debug

--- js/controller.js
+++ js/controller.js
@@ -2,7 +2,6 @@ var mainCtrl = function($scope) {
     $scope.playlists = [];
     $scope.searchPlaylist = function() {
         SC.get('/playlists', { q: $scope.keyword }, function(playlists) {
-            $scope.playlists.clear();
             $scope.playlists.push(playlists);
         });
     }
