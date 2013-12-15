f4413d75e6d9e8e99097aeb5c23deab11988cb57
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2013-12-15 19:10:49 +0900

    delete past results

--- js/controller.js
+++ js/controller.js
@@ -1,6 +1,9 @@
 var mainCtrl = function($scope) {
     $scope.playlists = [];
     $scope.searchPlaylist = function() {
+        for (var i in $scope.playlists) {
+            delete $scope.playlists[i]
+        }
         SC.get('/playlists', { q: $scope.keyword }, function(playlists) {
             for (var i in playlists) {
                 $scope.playlists.push(playlists[i]);
