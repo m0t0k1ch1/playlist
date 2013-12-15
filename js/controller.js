c5326d7a2d2ba4f7e6aa52ef338d424fbe31e6be
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2013-12-15 18:44:22 +0900

    show search results

--- js/controller.js
+++ js/controller.js
@@ -1,7 +1,7 @@
 var mainCtrl = function($scope) {
     $scope.searchPlaylist = function() {
         SC.get('/playlists', { q: $scope.keyword }, function(playlists) {
-            console.log(playlists);
+            $scope.playlists = playlists;
         });
     }
 }
