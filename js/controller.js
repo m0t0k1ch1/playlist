d59337bfdd376018371286403cf6678b1945198c
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2013-12-16 01:51:28 +0900

    show tracks

--- js/controller.js
+++ js/controller.js
@@ -1,11 +1,31 @@
 var mainCtrl = function($scope) {
     $scope.playlists = [];
+    $scope.tracks    = [];
+
     $scope.searchPlaylists = function() {
         $scope.playlists = [];
         SC.get('/playlists', { q: $scope.keyword }, function(playlists) {
             for (var i in playlists) {
                 $scope.playlists.push(playlists[i]);
             }
+            $scope.$apply();
+        });
+    }
+
+    $scope.showTracks = function(tracks) {
+        for (var i in tracks) {
+            $scope.tracks.push(tracks[i]);
+        }
+    }
+}
+
+var playlistCtrl = function($scope) {
+    $scope.getTracks = function() {
+        var mainScope = angular.element('#mainScope').scope();
+        var playlistId = $scope.playlistId;
+        SC.get('/playlists/' + playlistId + '.json', function(playlist) {
+            var tracks = playlist.tracks;
+            mainScope.showTracks(tracks);
             $scope.$apply();
         });
     }
