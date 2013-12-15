fd2240606f1fbe30f83bcb3603748c08ddd7cf9c
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2013-12-15 18:39:17 +0900

    use keyword

--- js/controller.js
+++ js/controller.js
@@ -1,7 +1,6 @@
 var mainCtrl = function($scope) {
     $scope.searchPlaylist = function() {
-        alert($scope.keyword);
-        SC.get('/playlists', { q: 'buskers' }, function(playlists) {
+        SC.get('/playlists', { q: $scope.keyword }, function(playlists) {
             console.log(playlists);
         });
     }
