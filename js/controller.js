3d76f9441c8244a3a3ccb80e6843c8aea5b5344e
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2013-12-15 18:57:47 +0900

    clear and push playlists

--- js/controller.js
+++ js/controller.js
@@ -1,7 +1,8 @@
 var mainCtrl = function($scope) {
     $scope.searchPlaylist = function() {
         SC.get('/playlists', { q: $scope.keyword }, function(playlists) {
-            $scope.playlists = playlists;
+            $scope.playlists.clear();
+            $scope.playlists.push(playlists);
         });
     }
 }
