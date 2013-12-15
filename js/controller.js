e321dc540332aad27a4634a0b847ccc8a40189ba
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2013-12-15 19:22:28 +0900

    fix

--- js/controller.js
+++ js/controller.js
@@ -1,7 +1,7 @@
 var mainCtrl = function($scope) {
     $scope.playlists = [];
     $scope.searchPlaylist = function() {
-        angular.element('searchResult').empty();
+        angular.element('#searchResult').empty();
         SC.get('/playlists', { q: $scope.keyword }, function(playlists) {
             for (var i in playlists) {
                 $scope.playlists.push(playlists[i]);
