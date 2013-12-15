32798f3cc5b4d0803a110d2156f4400480b5dbdf
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2013-12-16 00:33:33 +0900

    for debug

--- js/controller.js
+++ js/controller.js
@@ -2,13 +2,9 @@ var mainCtrl = function($scope) {
     $scope.playlists = [];
     $scope.searchPlaylists = function() {
         SC.get('/playlists', { q: $scope.keyword }, function(playlists) {
-            $scope.showPlaylists(playlists);
+            for (var i in playlists) {
+                $scope.playlists.push(playlists[i]);
+            }
         });
-    }
-    $scope.showPlaylists = function(playlists) {
-        for (var i in playlists) {
-            $scope.playlists.push(playlists[i]);
-        }
-        angular.element(document.getElementById('keyword')).val('');
     }
 }
