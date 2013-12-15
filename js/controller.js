cc665d16267334717026d1c3e4ae24ebd03c6adb
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2013-12-15 19:01:29 +0900

    define empty array

--- js/controller.js
+++ js/controller.js
@@ -1,4 +1,5 @@
 var mainCtrl = function($scope) {
+    $scope.playlists = [];
     $scope.searchPlaylist = function() {
         SC.get('/playlists', { q: $scope.keyword }, function(playlists) {
             $scope.playlists.clear();
