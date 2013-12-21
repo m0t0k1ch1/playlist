9a399c2c6a9b3bae864280942986e98cce6b68fb
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2013-12-21 14:11:31 +0900

    for debug

--- js/app.js
+++ js/app.js
@@ -1,26 +1,17 @@
 var mainCtrl = function($scope)
 {
+  $scope.playlists = [];
   $scope.searchPlaylists = function() {
-    $scope.playlists = [];
     SC.get('/playlists', { q: $scope.keyword }, function(playlists) {
-      var playlistScopeElement, playlistScope;
       for (var i in playlists) {
         var playlist = playlists[i];
         $scope.playlists.push(playlist);
         $scope.$apply();
-        playlistScopeElement     = document.getElementById('playlist' + i);
-        playlistScope            = angular.element(playlistScopeElement).scope();
+        var playlistScopeElement = document.getElementById('playlist' + i);
+        var playlistScope        = angular.element(playlistScopeElement).scope();
         playlistScope.playlistId = playlist.id;
       }
     });
-  }
-
-  $scope.showTracks = function(tracks) {
-    $scope.tracks = [];
-    for (var i in tracks) {
-      $scope.tracks.push(tracks[i]);
-    }
-    $scope.$apply();
   }
 }
 
@@ -28,11 +19,47 @@ var playlistCtrl = function($scope)
 {
   $scope.getTracks = function() {
     SC.get('/playlists/' + $scope.playlistId, {}, function(playlist) {
-      var tracks           = playlist.tracks;
-      var mainScopeElement = document.getElementById('mainScope');
-      var mainScope        = angular.element(mainScopeElement).scope();
-      mainScope.showTracks(tracks);
+      var tracks = playlist.tracks;
+      tracksScope.showTracks(tracks);
     });
+  }
+}
+
+var tracksCtrl = function($scope)
+{
+  $scope.index      = 0;
+  $scope.repeatMode = 'song';
+  $scope.tracks     = [];
+
+  $scope.showTracks = function(tracks) {
+    for (var i in tracks) {
+      $scope.tracks.push(tracks[i]);
+    }
+    $scope.$apply();
+  }
+
+  $scope.play = function() {
+    var trackScopeElement = document.getElementById('track' + $scope.index);
+    var trackScope        = angular.element(trackScopeElement).scope();
+    trackScope.setTrack();
+  }
+
+  $scope.prev = function() {
+    $scope.index--;
+    $scope.play();
+  }
+
+  $scope.next = function() {
+    $scope.index++;
+    $scope.play();
+  }
+
+  $scope.repeatSong = function() {
+    $scope.repeatMode = 'song';
+  }
+
+  $scope.repeatPlaylist = function() {
+    $scope.repeatMode = 'playlist';
   }
 }
 
