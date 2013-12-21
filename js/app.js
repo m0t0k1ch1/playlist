b53d817fad9921157bcdf1129dcf979a6e511166
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2013-12-21 15:34:15 +0900

    fix

--- js/app.js
+++ js/app.js
@@ -23,11 +23,11 @@ var mainCtrl = function($scope)
     SC.get('/playlists', { q: $scope.keyword }, function(playlists) {
       $scope.playlists = [];
       for (var i in playlists) {
-        var playlist             = playlists[i];
-        var playlistScopeElement = document.getElementById('playlist' + i);
-        var playlistScope        = angular.element(playlistScopeElement).scope();
+        var playlist = playlists[i];
         $scope.playlists.push(playlist);
         $scope.$apply();
+        var playlistScopeElement = document.getElementById('playlist' + i);
+        var playlistScope        = angular.element(playlistScopeElement).scope();
         playlistScope.playlistId = playlist.id;
       }
     });
@@ -41,7 +41,8 @@ var playlistCtrl = function($scope)
 
   $scope.getTracks = function() {
     SC.get('/playlists/' + $scope.playlistId, {}, function(playlist) {
-      tracksScope.showTracks(playlist.tracks);
+      var tracks = playlist.tracks;
+      tracksScope.showTracks(tracks);
     });
   }
 }
