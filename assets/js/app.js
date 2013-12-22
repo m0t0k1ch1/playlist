4d20fa848f884dbba7781de5968b8dbd23a01004
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2013-12-22 09:18:40 +0900

    fix

--- assets/js/app.js
+++ assets/js/app.js
@@ -17,11 +17,14 @@ var mainCtrl = function($scope)
     }
   });
 
+  var tracksScope = angular.element('#tracks').scope();
+
   $scope.playlists = [];
 
   $scope.searchPlaylists = function() {
     SC.get('/playlists', { q: $scope.keyword }, function(playlists) {
       $scope.playlists   = [];
+      tracksScope.tracks = [];
       for (var i in playlists) {
         var playlist = playlists[i];
         $scope.playlists.push(playlist);
@@ -52,7 +55,6 @@ var tracksCtrl = function($scope)
   $scope.showTracks = function(tracks) {
     mainScope.playlists = [];
     $scope.tracks       = [];
-    $scope.maxIndex     = tracks.length - 1;
     for (var i in tracks) {
       $scope.tracks.push(tracks[i]);
     }
